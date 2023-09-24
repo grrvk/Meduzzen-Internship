b749e80da14b10bc722ebe0cae86bdcedd5d7463
vika <vika@granadzer.com>
2023-09-24 18:35:26 +0300

    Fourth-Commit-Models-Schemas-Alembic

---
+++ app/models/model.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, DateTime
+from app.db.database import Base
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    __tablename__ = "User"
+
+    user_id = Column(Integer, primary_key=True)
+    user_email = Column(String, nullable=False)
+    user_firstname = Column(String, nullable=False)
+    user_lastname = Column(String, nullable=False)
+    user_status = Column(String, nullable=True)
+    user_city = Column(String, nullable=True)
+    user_phone = Column(String, nullable=True)
+    user_avatar = Column(String, nullable=True)
+    hashed_password: str = Column(String, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+
